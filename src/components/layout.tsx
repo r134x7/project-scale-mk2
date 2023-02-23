83b2a036e878ef90995eed14f2ecde1d17d31d2b
r134x7 <r134x7@gmx.com>
2023-02-23 18:02:15 +1100

    setting up a layout component

---
+++ src/components/layout.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Layout() {
+
+    // need to create a layout to wrap around the _app.tsx return statement to persist the layout between pages
+    // source: https://nextjs.org/docs/basic-features/layouts
+    return (
+        <>
+        
+        </>
+    )
+}
