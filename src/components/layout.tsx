01a2a2fdfb07b0477b172c022d2cdace5cb7ae74
r134x7 <r134x7@gmx.com>
2023-02-23 22:56:33 +1100

    got a layout inserted into app... CSS seems to be separated...

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,40 @@
-
+import { Menu } from "@headlessui/react"
+import Link from "next/link"
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Layout() {
 
+    const { data: sessionData } = useSession();
     // need to create a layout to wrap around the _app.tsx return statement to persist the layout between pages
     // source: https://nextjs.org/docs/basic-features/layouts
     return (
-        <>
-        
-        </>
+        <Menu>
+            <Menu.Button>Menu</Menu.Button>
+            <Menu.Items>
+                <Menu.Item>
+                    {({active}) => (
+                        <Link className={`${active ? 'bg-blue-500' : "bg-red-400"}`} href="/">
+                            Home
+                        </Link>
+                    )}
+                </Menu.Item>
+                <Menu.Item>
+                    {({active}) => (
+                        <Link className={`${active ? 'bg-blue-500' : "bg-red-400"}`} href="/">
+                            User
+                        </Link>
+                    )}
+                </Menu.Item>
+                <Menu.Item>
+                    {({active}) => (
+                        <button className={`${active ? 'bg-blue-500' : "bg-red-400"}`}
+                        onClick={sessionData ? () => void signOut() : () => void signIn()}
+                        >
+                            {sessionData ? "Sign out" : "Sign in"}
+                        </button>
+                    )}
+                </Menu.Item>
+            </Menu.Items>
+        </Menu>
     )
 }
