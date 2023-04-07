9f63d3bd81d0928d82d0eb47548fc235c030fa01
r134x7 <r134x7@gmx.com>
2023-04-07 21:44:16 +1000

    migrated to mySQL, lost data from SQLite usage, changed journal and dailyplan to use varChar to prevent error when creating records and ambitions

--- prisma/migrations/20230323081441_adding_user_name_to_ambitions/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Ambitions" ADD COLUMN "userName" TEXT;
---
+++ prisma/migrations/20230407113802_adding_var_char/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `ambitions` MODIFY `dailyPlan` VARCHAR(1000) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `record` MODIFY `journal` VARCHAR(200) NOT NULL;
